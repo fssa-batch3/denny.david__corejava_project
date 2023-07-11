bf932f3bc517bc6723f9d3e8fee192dfa7b0fda8
Denny Franklin <116251491+dennyfranklin@users.noreply.github.com>
2023-07-11 14:03:20 +0530

    Add files via upload

---
+++ src/main/java/com/fssa/denny/day3/practice/TestEmployee.java
@@ -0,0 +1,21 @@
+package com.fssa.denny.day3.practice;
+
+class Employee {
+	public int id;
+    public String name;
+ public Employee(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+   
+}
+
+public class TestEmployee {
+	 
+    public static void main(String[] args) {
+        Employee e1 = new Employee(1, "naresh");
+        System.out.println(e1.id+e1.name);
+        Employee e2 = new Employee(2, "suresh");
+        System.out.println(e2.id+e2.name);
+    }
+}
